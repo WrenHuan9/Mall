769ad591cc4d41dfff49bc154e641401c69352ac
WrenHuan9 <wren951017@gmail.com>
2025-02-15 02:14:09 +0100

    First commit for env.

---
+++ Coupon/src/test/java/com/noctis/mall/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noctis.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Order/src/test/java/com/noctis/mall/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noctis.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
